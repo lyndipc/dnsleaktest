0dd28170d10e2b273e99227cdeba5fb5461fda19
Lyndi Castrejon <lyndic@protonmail.com>
2022-10-21 03:17:40 -0600

    Remove argument and parse json

--- dnsleaktest.php
+++ dnsleaktest.php
@@ -6,8 +6,25 @@
         'Content-Type: application/json'
     );
 
-    curl_setopt($ch, CURLOPT_URL, "https://json.geoiplookup.io/${argv[1]}");
+    curl_setopt($ch, CURLOPT_URL, "https://json.geoiplookup.io/");
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
     $res = curl_exec($ch);
+    curl_close($ch);
+
+    $output = json_decode($res);
+
+    echo "************************************************* \n";
+    echo "Your DNS leak test results: \n\n";
+
+    echo "IP: ";
+    echo $output->ip . "\n";
+    echo "ISP: ";
+    echo $output->isp . "\n";
+    echo "Hostname: ";
+    echo $output->hostname . "\n";
+    echo "Location: ";
+    echo $output->city . ", " . $output->country_name ."\n";
+?>
