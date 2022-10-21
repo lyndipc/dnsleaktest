ed7086a5cad45414f82b34d7c4dd7b8108fe1c31
Lyndi Castrejon <lyndic@protonmail.com>
2022-10-21 02:35:33 -0600

    Accept optional ip as argument

--- dnsleaktest.php
+++ dnsleaktest.php
@@ -1,12 +1,12 @@
 <?php
-    
+
     $ch = curl_init();
     $headers = array(
         'Accept: application/json',
         'Content-Type: application/json'
     );
 
-    curl_setopt($ch, CURLOPT_URL, "https://json.geoiplookup.io/37.19.221.142");
+    curl_setopt($ch, CURLOPT_URL, "https://json.geoiplookup.io/${argv[1]}");
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($ch, CURLOPT_HEADER, 0);
 
